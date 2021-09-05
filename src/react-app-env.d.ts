40cc2c401a4c9a29d4d150250b3f7355a066c88f
Swedish li <one.piece.li.2016@gmail.com>
2021-09-05 21:20:28 +0900

    update types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="react-scripts" />
-
-import "styled-components";
-
-// and extend them!
-declare module "styled-components" {
-  export interface DefaultTheme {
-    primary: {
-      color: string;
-      backgroundColor: string;
-    };
-  }
-}
---
+++ src/types/style.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+// and extend them!
+declare module "styled-components" {
+  export interface DefaultTheme {
+    primary: {
+      color: string;
+      backgroundColor: string;
+    };
+  }
+}
